846755b738d735e053c4b0c56152482547fdc733
Nguyen Van Dung <dung.nguyen@pn.com.vn>
2017-03-22 16:48:32 +0700

    dung commit

--- mailet/standard/src/test/java/org/apache/james/transport/matchers/SMTPAuthSuccessfulTest.java
+++ mailet/standard/src/test/java/org/apache/james/transport/matchers/SMTPAuthSuccessfulTest.java
@@ -22,6 +22,10 @@ package org.apache.james.transport.matchers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Collection;
+
+import org.apache.mailet.MailAddress;
+import org.apache.mailet.base.test.FakeMail;
 import org.apache.mailet.base.test.FakeMailContext;
 import org.apache.mailet.base.test.FakeMatcherConfig;
 import org.junit.Before;
@@ -35,12 +39,13 @@ public class SMTPAuthSuccessfulTest {
     public void setUp() throws Exception {
         testee = new SMTPAuthSuccessful();
         testee.init(FakeMatcherConfig.builder()
+        		.matcherName("matcherName")
             .mailetContext(FakeMailContext.defaultContext())
             .build());
     }
 
     @Test
-    public void matchShouldReturnRecipientsWhenAuthUserAttributeIsPresent() {
+    public void matchShouldReturnRecipientsWhenAuthUserAttributeIsPresent()  throws Exception{
         /*
         Question 1
 
@@ -50,10 +55,18 @@ public class SMTPAuthSuccessfulTest {
 
         As a result, the recipient should be returned
          */
+    	
+    	FakeMail fakeMail = FakeMail.builder()
+    		   	.recipients(new MailAddress("dung.nguyen@gmail.com"))
+    		   	.attribute("org.apache.james.SMTPAuthUser", "value")
+    		   	.build();
+    		   	
+    	Collection<MailAddress> result = testee.match(fakeMail);
+    	assertThat(result).containsOnly(new MailAddress("dung.nguyen@gmail.com"));
     }
 
     @Test
-    public void matchShouldNotReturnRecipientsWhenAuthUserAttributeIsAbsent() {
+    public void matchShouldNotReturnRecipientsWhenAuthUserAttributeIsAbsent() throws Exception{
         /*
         Question 2
 
@@ -63,6 +76,13 @@ public class SMTPAuthSuccessfulTest {
 
         As a result, the recipient should not be returned
          */
+    	FakeMail fakeMail2 = FakeMail.builder()
+    		   	.recipients(new MailAddress("dung.nguyen@gmail.com"))
+    		   	.build();
+    		   	
+    	Collection<MailAddress> result = testee.match(fakeMail2);
+    	assertThat(result).isNull();
+    	
     }
 
 }
